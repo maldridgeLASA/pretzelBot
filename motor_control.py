d14e4fe9da7b338a74097c78f1d4bd7270af8fd3
Michael Aldridge <maldridge@blackmac.(none)>
2013-07-29 20:51:54 -0500

    removed defunct main method renamed start to init

--- motor_control.py
+++ motor_control.py
@@ -9,7 +9,7 @@ RBAK = 201
 
 
 class pretzelRobot:
-  def setup_link(self, port):
+  def __init__(self, port):
     ser = serial.Serial(port, 115200)
     print "using port", ser.portstr
 
@@ -22,19 +22,3 @@ class pretzelRobot:
       ser.write(str(RFWD)+str(rspeed))
     else:
       ser.write(str(202)+str(127))
-
-  def main(self):
-    #setup_link(raw_input("Port? "))
-    self.setup_link('/dev/ttyUSB0')
-    #left=int(raw_input("Left Speed? "))
-    #right=int(raw_input("Right Speed? "))
-    drive(127,127)
-    raw_input("Press a key to stop")
-    drive(0,0)
-    exit()
-
-
-r = pretzelRobot
-
-r.main(self)
-
